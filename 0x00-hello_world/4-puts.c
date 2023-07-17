e0597af7ea8d167d367380e7653739141275bbff
Assiiim <oussinaasmaa454@gmail.com>
2023-07-17 08:05:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,10 @@
 *return: always 0 (success)
 */
 int main(void)
+/**
+*main - a program that prints a line with puts function
+*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
