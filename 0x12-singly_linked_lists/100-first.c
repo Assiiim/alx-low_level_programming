699670d7746e2c4b67d1e63edf87d08241eba727
Oussina Asmaa <oussinaasmaa454@gmail.com>
2024-02-12 15:25:21 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - priting before the main
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house uponmy back!\n");
+}
